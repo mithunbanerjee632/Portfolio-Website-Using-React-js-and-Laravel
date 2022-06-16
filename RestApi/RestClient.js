4af002567efc664aa77e55f4a7bd41ea14744f8b
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-06-16 12:50:04 +0600

    working with axiox

---
+++ RestApi/RestClient.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+class RestClient {
+
+
+    static GetRequest(GetUrl){
+        axios.get(GetUrl).then(response=>{
+            return response.data;
+        }).catch(error=>{
+            return null;
+        })
+    }
+
+}
+
+export default RestClient;
